b9ff06f27e7ba6901e60c326ff6136ff89187d79
Arwaaq <arwa.itt@gmail.com>
2018-04-05 18:03:10 +0300

    Initial Commit

---
+++ gitt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitt
+//
+//  Created by Arwa  on 4/5/18.
+//  Copyright © 2018 Arwa . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
